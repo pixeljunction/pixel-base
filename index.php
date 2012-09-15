ed110ecbaad953fbf4c5dfe306e7be97b5ef3c28
Mark Wilkinson <mark@markwilkinson.me>
2012-09-15 13:07:53 +0100

    Main Content Loop Added and Index Changed to Use It
    Main content template part used inside the loop. Handles single and
    archive pages. Index.php altered to use the content loop/

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php get_header() ?>
 
-<p class="theme-welcome">This theme really does very little. It contains a good framework of options in order to build new themes upon. Therefore please create a child theme to use with this theme.</p>
+	<?php
+		
+		/* if we have posts in the loop */
+		if( have_posts() ) {
+			
+			/* begin the post loop */
+			while ( have_posts() ) : the_post();
+				
+				/* get the content template part based on the post format */
+				get_template_part( 'loops/content', get_post_format() );
+				
+			/* end the loop */
+			endwhile;
+			
+			/* show the navigation for pageination */
+			echo pxjn_content_nav();
+		
+		}
+	
+	?>
 
 <?php get_footer(); ?>
