23fca4ae870204aef365370148ec9d6ba1f04a96
Mark Wilkinson <hello@markwilkinson.me>
2013-04-07 17:43:38 +0100

    Commented some Functions
    Commented some functions to help understanding.

--- options.php
+++ options.php
@@ -1,10 +1,15 @@
 <?php
-/**
- * A unique identifier is defined to store the options in the database and reference them from the theme.
- * By default it uses the theme name, in lowercase and without spaces, but this can be changed if needed.
- * If the identifier changes, it'll appear as if the options have been reset.
- */
 
+/*
+* This file declares the options to add to the theme options
+* and sets the name of the options to which they are stored
+* within.
+*/
+
+/***************************************************************
+* Function optionsframework_option_name()
+* Defines the name to use in which to store the options
+***************************************************************/
 function optionsframework_option_name() {
 
 	/* sets up the name of the options in the database */
