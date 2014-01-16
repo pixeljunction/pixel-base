6e7c805a2c2d4962720f729c11bdf462576833e9
Mark Wilkinson <mark@markwilkinson.me>
2014-01-16 20:21:45 +0000

    Renamed Default Widget Area
    Also added core styles back to widget areas.

--- functions/widgets.php
+++ functions/widgets.php
@@ -9,12 +9,12 @@ function pxjn_register_widgets() {
 	/* repeat for each sidebar to be registered */
 	register_sidebar(
 		array(
-			'id' => 'pxjn_posts_widget',
+			'id' => 'pxjn_posts_sidebar',
 			'name' => __( 'Posts Widget Area' ),
 			'description' => __( 'This widget area shows on the posts pages.' ),
-			'before_widget' => '<div id="%1$s" class="pxjn-widget %2$s">',
+			'before_widget' => '<div id="%1$s" class="widget pxjn_widget %2$s">',
 			'after_widget' => '</div>',
-			'before_title' => '<h3 class="pxjn-widget-title">',
+			'before_title' => '<h3 class="widget_title pxjn_widget_title">',
 			'after_title' => '</h3>'
 		)
 	);
